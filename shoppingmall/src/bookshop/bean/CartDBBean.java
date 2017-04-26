df2668a60f2ff3323eb0168e15202dc7f098505e
seungchul <seungceal@gmail.com>
2017-04-26 16:24:53 +0900

    승철

--- shoppingmall/src/bookshop/bean/CartDBBean.java
+++ shoppingmall/src/bookshop/bean/CartDBBean.java
@@ -97,6 +97,7 @@ public class CartDBBean {
 				e.printStackTrace();
 			}
 		}
+		return x;
 	}
 	
 	
--- shoppingmall/src/bookshop/bean/CartDataBean.java
+++ shoppingmall/src/bookshop/bean/CartDataBean.java
@@ -10,4 +10,48 @@ public class CartDataBean {
 	private int buy_price; //판매가
 	private byte buy_count; //판매수량
 	private String book_image; //책 이미지
+	public int getCart_id() {
+		return cart_id;
+	}
+	public void setCart_id(int cart_id) {
+		this.cart_id = cart_id;
+	}
+	public String getBuyer() {
+		return buyer;
+	}
+	public void setBuyer(String buyer) {
+		this.buyer = buyer;
+	}
+	public int getBook_id() {
+		return book_id;
+	}
+	public void setBook_id(int book_id) {
+		this.book_id = book_id;
+	}
+	public String getBook_title() {
+		return book_title;
+	}
+	public void setBook_title(String book_title) {
+		this.book_title = book_title;
+	}
+	public int getBuy_price() {
+		return buy_price;
+	}
+	public void setBuy_price(int buy_price) {
+		this.buy_price = buy_price;
+	}
+	public byte getBuy_count() {
+		return buy_count;
+	}
+	public void setBuy_count(byte buy_count) {
+		this.buy_count = buy_count;
+	}
+	public String getBook_image() {
+		return book_image;
+	}
+	public void setBook_image(String book_image) {
+		this.book_image = book_image;
+	}
+	
+	
 }
